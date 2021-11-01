8cb7b64ed72d6817af0a04896b91b4d3999e023c
AlanaZUP <alana.reis@zup.com.br>
2021-11-01 11:10:32 -0300

    DTO para consulta das transações

--- extrato/src/main/java/com/dojao/extrato/consultaExtrato/TransacaoResponse.java
+++ extrato/src/main/java/com/dojao/extrato/consultaExtrato/TransacaoResponse.java
@@ -1,6 +1,38 @@
 package com.dojao.extrato.consultaExtrato;
 
-public class ExtratosResponse {
+import com.dojao.extrato.transacao.Operacao;
+import com.dojao.extrato.transacao.Transacao;
 
-    private 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransacaoResponse {
+
+    private Long id;
+    private Operacao operacao;
+    private BigDecimal valor;
+    private LocalDateTime dataTransacao;
+
+    public TransacaoResponse(Transacao transacao) {
+        this.id = transacao.getId();
+        this.operacao = transacao.getOperacao();
+        this.valor = transacao.getValor();
+        this.dataTransacao = transacao.getDataTransacao();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Operacao getOperacao() {
+        return operacao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public LocalDateTime getDataTransacao() {
+        return dataTransacao;
+    }
 }
