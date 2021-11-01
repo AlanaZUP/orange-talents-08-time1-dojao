596d31160cff530f704540a6bb7ac61da6d3b7be
AlanaZUP <alana.reis@zup.com.br>
2021-11-01 11:11:02 -0300

    Teste basico endpoint de consulta

--- extrato/src/test/java/com/dojao/extrato/consultaExtrato/ConsultaControllerTest.java
+++ extrato/src/test/java/com/dojao/extrato/consultaExtrato/ConsultaControllerTest.java
@@ -1,4 +1,36 @@
+package com.dojao.extrato.consultaExtrato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import com.google.gson.Gson;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ConsultaControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Gson gson = new Gson();
+
+    @Test
+    public void deveRetornarTransacoes() throws Exception {
+        mockMvc.perform(get("/conta/1/extrato")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+
+    }
 }
